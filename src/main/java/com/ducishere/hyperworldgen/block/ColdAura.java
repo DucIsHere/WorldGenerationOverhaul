774f1ecf2a4e022ce9e4389bfe406ccfcef05a8f
DucIsHere <thelegendwolf07@gmail.com>
2025-08-17 13:22:49 +0700

    Update ColdAura.java

--- src/main/java/com/ducishere/hyperworldgen/block/ColdAura.java
+++ src/main/java/com/ducishere/hyperworldgen/block/ColdAura.java
@@ -1,26 +1,23 @@
 package com.ducishere.hyperworldgen.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.particle.ParticleTypes;
 
 public class ColdAura {
 
-    public static void tickSoulLantern(World world, BlockPos pos) {
-        if (world.isClient) return;
+    public static void tick(World world, BlockPos pos){
+        if(world.isClient) return;
 
-        int radius = 5; // bán kính 5 block
-        for (PlayerEntity player : world.getPlayers()) {
-            if (player.getBlockPos().isWithinDistance(pos, radius)) {
-                // Áp hiệu ứng slowness + giảm nhiệt độ (custom stat nếu có)
+        int radius = 5;
+        for(PlayerEntity player : world.getPlayers()){
+            if(player.getBlockPos().isWithinDistance(pos, radius)){
                 player.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 40, 1, true, false));
-                // Nếu có system temperature: giảm nhiệt độ player ở đây
+                // Particle lạnh
+                world.addParticle(ParticleTypes.SNOWFLAKE, player.getX(), player.getY() + 1, player.getZ(), 0, 0.01, 0);
             }
         }
     }
