7579202461f36e976fe260009c46db2e8a589e15
DucIsHere <thelegendwolf07@gmail.com>
2025-08-17 20:13:14 +0700

    Update MainMod.java

--- src/main/java/com/ducishere/hyperworldgen/thirst/MainMod.java
+++ src/main/java/com/ducishere/hyperworldgen/thirst/MainMod.java
@@ -1,16 +1,43 @@
 package com.ducishere.hyperworldgen.thirst;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.api.ClientModInitializer;
-import com.ducishere.hyperworldgen.thirst.network.PacketHandler;
-import com.ducishere.hyperworldgen.thirst.system.StatusManager;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.server.level.ServerPlayer;
 
-public class MainMod implements ModInitializer, ClientModInitializer {
+public class HyperWorldGenThirst implements ModInitializer {
+
     @Override
     public void onInitialize() {
-    }
+        // Server tick event
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            server.getPlayerList().getPlayers().forEach(player -> ThirstHandler.onPlayerTick(player));
+        });
 
-    @Override
-    public void onInitializeClient() {
+        // Hand drinking
+        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+            ThirstHandler.onUseBlock(new PlayerInteractEvent.UseBlock(player, hand, hitResult));
+            return null;
+        });
+
+        UseItemCallback.EVENT.register((player, world, hand) -> {
+            ThirstHandler.onUseEmpty(new PlayerInteractEvent.UseEmpty(player, hand));
+            return null;
+        });
+
+        // Client tick for hand drinking cooldown
+        ClientTickEvents.END_CLIENT_TICK.register(client -> ThirstHandler.onClientTick(null));
+
+        // Optional: sync thirst on player join
+        ServerLifecycleEvents.PLAYER_JOIN.register((player, server) -> {
+            if (player instanceof ServerPlayer serverPlayer)
+                ThirstHandler.syncThirst(serverPlayer);
+        });
+
+        System.out.println("[HyperWorldGenThirst] Thirst module initialized!");
     }
 }
