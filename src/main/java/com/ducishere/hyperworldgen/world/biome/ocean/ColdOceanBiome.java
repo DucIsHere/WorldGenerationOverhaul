c03887642e0adb08243f937996d73e1b1c918be6
DucIsHere <thelegendwolf07@gmail.com>
2025-08-17 10:28:23 +0700

    Create ColdOceanBiome.java

---
+++ src/main/java/com/ducishere/hyperworldgen/world/biome/ocean/ColdOceanBiome.java
@@ -0,0 +1,21 @@
+package yourmodid.biome;
+
+import net.minecraft.world.biome.*;
+
+public class ColdOceanBiome {
+    public static Biome create() {
+        return new Biome.Builder()
+                .precipitation(true)
+                .temperature(0.3f)
+                .downfall(0.9f)
+                .effects(new BiomeEffects.Builder()
+                        .waterColor(4020182)
+                        .waterFogColor(329011)
+                        .fogColor(12638463)
+                        .skyColor(8037887)
+                        .build())
+                .spawnSettings(new SpawnSettings.Builder().build())
+                .generationSettings(new GenerationSettings.Builder().build())
+                .build();
+    }
+}
