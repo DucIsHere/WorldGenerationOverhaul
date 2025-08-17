afe376dcb141cac765e7975c5abc8e47a4d60eb3
DucIsHere <thelegendwolf07@gmail.com>
2025-08-17 10:28:48 +0700

    Create WarmOceanBiome.java

---
+++ src/main/java/com/ducishere/hyperworldgen/world/biome/ocean/WarmOceanBiome.java
@@ -0,0 +1,21 @@
+package yourmodid.biome;
+
+import net.minecraft.world.biome.*;
+
+public class WarmOceanBiome {
+    public static Biome create() {
+        return new Biome.Builder()
+                .precipitation(true)
+                .temperature(1.0f)
+                .downfall(0.8f)
+                .effects(new BiomeEffects.Builder()
+                        .waterColor(4445678)
+                        .waterFogColor(270131)
+                        .fogColor(12638463)
+                        .skyColor(8037887)
+                        .build())
+                .spawnSettings(new SpawnSettings.Builder().build())
+                .generationSettings(new GenerationSettings.Builder().build())
+                .build();
+    }
+}
