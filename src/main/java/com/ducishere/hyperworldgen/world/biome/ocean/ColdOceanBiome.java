a3ccf80daa1dc3281afbdde9e73a0df663201d16
DucIsHere <thelegendwolf07@gmail.com>
2025-08-17 10:32:13 +0700

    Update ColdOceanBiome.java

--- src/main/java/com/ducishere/hyperworldgen/world/biome/ocean/ColdOceanBiome.java
+++ src/main/java/com/ducishere/hyperworldgen/world/biome/ocean/ColdOceanBiome.java
@@ -1,9 +1,19 @@
 package com.ducishere.hyperworldgen.world.biome.ocean;
 
 import net.minecraft.world.biome.*;
+import net.minecraft.world.gen.feature.DefaultBiomeFeatures;
 
 public class ColdOceanBiome {
     public static Biome create() {
+        SpawnSettings.Builder spawn = new SpawnSettings.Builder();
+        spawn.creatureSpawnProbability(0.1f);
+        DefaultBiomeFeatures.addOceanMobs(spawn, 10, 2, 4);
+
+        GenerationSettings.Builder gen = new GenerationSettings.Builder();
+        DefaultBiomeFeatures.addOceanCarvers(gen);
+        DefaultBiomeFeatures.addOceanStructures(gen);
+        DefaultBiomeFeatures.addDefaultOres(gen);
+
         return new Biome.Builder()
                 .precipitation(true)
                 .temperature(0.3f)
@@ -14,8 +24,8 @@ public class ColdOceanBiome {
                         .fogColor(12638463)
                         .skyColor(8037887)
                         .build())
-                .spawnSettings(new SpawnSettings.Builder().build())
-                .generationSettings(new GenerationSettings.Builder().build())
+                .spawnSettings(spawn.build())
+                .generationSettings(gen.build())
                 .build();
     }
 }
