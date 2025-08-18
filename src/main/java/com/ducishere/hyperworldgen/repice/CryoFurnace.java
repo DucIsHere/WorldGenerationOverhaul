84422e8ab9f37fe8c09fd3f36a5ab19f28726957
DucIsHere <thelegendwolf07@gmail.com>
2025-08-18 20:47:12 +0700

    Create CryoFurnace.java

---
+++ src/main/java/com/ducishere/hyperworldgen/repice/CryoFurnace.java
@@ -0,0 +1,27 @@
+package com.ducishere.hyperworldgen.recipe;
+
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.SmeltingRecipe;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import hyperworldgen.item.ModItems;
+
+public class ModRecipes {
+    public static void registerRecipes() {
+        // Furnace Smelting: Steel Abyss Ore -> Steel Abyss Scrap
+        SmeltingRecipe steelAbyssSmelting = new SmeltingRecipe(
+            new Identifier("hyperworldgen", "steel_abyss_smelting"),
+            "", // group
+            new ItemStack(ModItems.STEEL_ABYSS_SCRAP),
+            ModItems.STEEL_ABYSS_ORE.getDefaultStack(),
+            1.0f, // experience
+            40000  // cook time in ticks
+        );
+
+        Registry.register(Registry.RECIPE_SERIALIZER, 
+                          new Identifier("hyperworldgen", "steel_abyss_smelting"), 
+                          RecipeSerializer.SMELTING);
+    }
+}
