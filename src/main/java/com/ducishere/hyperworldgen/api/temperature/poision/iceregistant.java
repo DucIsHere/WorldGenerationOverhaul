4d062db85cf938af1e0883432a971efc88b5041a
DucIsHere <thelegendwolf07@gmail.com>
2025-08-19 14:16:41 +0700

    Create iceregistant.java

---
+++ src/main/java/com/ducishere/hyperworldgen/api/temperature/poision/iceregistant.java
@@ -0,0 +1,8 @@
+package toughasnails.api.potion;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class TANEffects {
+    public static final MobEffect ICE_RESISTANCE = new MobEffect(MobEffectCategory.BENEFICIAL, 0x99ccff);
+}
