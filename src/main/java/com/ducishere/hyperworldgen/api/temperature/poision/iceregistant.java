1504c0f0e1603fbb6bd94a2b7c783c52c375295f
DucIsHere <thelegendwolf07@gmail.com>
2025-08-21 20:19:00 +0700

    Delete src/main/java/com/ducishere/hyperworldgen/api/temperature directory

--- src/main/java/com/ducishere/hyperworldgen/api/temperature/poision/iceregistant.java
+++
@@ -1,8 +0,0 @@
-package toughasnails.api.potion;
-
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffectCategory;
-
-public class TANEffects {
-    public static final MobEffect ICE_RESISTANCE = new MobEffect(MobEffectCategory.BENEFICIAL, 0x99ccff);
-}
