8e437987eb19ad229c4e542b469c9826ac6e06a2
DucIsHere <thelegendwolf07@gmail.com>
2025-08-22 11:20:33 +0700

    Update ThunderHeights.java

--- src/main/java/com/ducishere/hyperworldgen/world/biome/mountain/ThunderHeights.java
+++ src/main/java/com/ducishere/hyperworldgen/world/biome/mountain/ThunderHeights.java
@@ -16,10 +16,16 @@ public class ThunderHeights {
             .moodSound(BiomeMoodSound.CAVE)
             .build();
 
+        BiomeSpawnSettings.Builder spawnBuilder = new BiomeSpawnSettings.Builder();
+        // Ít mob, chủ yếu là mob sống sót trong điều kiện khắc nghiệt
+        // Tùy bạn thêm custom mob
+
+        GenerationSettings.Builder generationSettings = new GenerationSettings.Builder();
+
         return new Biome.Builder()
             .precipitation(Biome.Precipitation.RAIN) // núi mưa thường
-            .temperature(0.3f) // lạnh vừa
-            .downfall(0.6f)   // rainfall vừa phải
+            .temperature(-1.5f) // lạnh vừa
+            .downfall(1.9f)   // rainfall vừa phải
             .effects(effects)
             .spawnSettings(IronridgeSpawns.create())      // mobs
             .generationSettings(IronridgeGeneration.create()) // ores + terrain features
