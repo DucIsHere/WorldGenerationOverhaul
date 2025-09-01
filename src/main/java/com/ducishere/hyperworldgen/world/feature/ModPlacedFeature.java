77a75c5117f6787dcb12f97de935f784561930e9
DucIsHere <thelegendwolf07@gmail.com>
2025-09-01 17:56:48 +0700

    Update ModPlacedFeature.java

--- src/main/java/com/ducishere/hyperworldgen/world/feature/ModPlacedFeature.java
+++ src/main/java/com/ducishere/hyperworldgen/world/feature/ModPlacedFeature.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class ModPlacedFeatures {
 
     public static final RegistryKey<PlacedFeature> SNOW_LAYER_8_PLACED = registerKey("snow_layer_8_placed");
+    public static final RegistryKey<PlacedFeature> WILD_RICE_PLACED = registerKey("wild_rice_placed");
 
     public static void bootstrap(Registerable<PlacedFeature> context) {
         context.register(SNOW_LAYER_8_PLACED, new PlacedFeature(
@@ -24,6 +25,16 @@ public class ModPlacedFeatures {
                         CountPlacementModifier.of(8), // số lần spawn
                         InSquarePlacementModifier.spread(),
                         HeightmapPlacementModifier.of()
+                )
+        ));
+
+        context.register(WILD_RICE_PLACED, new PlacedFeature(
+                context.getHolderOrThrow(ModConfiguredFeatures.WILD_RICE),
+                List.of(
+                        CountPlacementModifier.of(10),
+                        InSquarePlacementModifier.spread(),
+                        HeightmapPlacementModifier.of()
+                        // Nếu có BiomeFilter.biome() thì thêm vào, còn không có thì có thể bỏ qua!
                 )
         ));
     }
