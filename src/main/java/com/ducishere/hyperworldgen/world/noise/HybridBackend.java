7c6412a9b2e6e2d5440d757748e9a694c9c14a7f
DucIsHere <thelegendwolf07@gmail.com>
2025-09-07 14:10:23 +0700

    Update HybridBackend.java

--- src/main/java/com/ducishere/hyperworldgen/world/noise/HybridBackend.java
+++ src/main/java/com/ducishere/hyperworldgen/world/noise/HybridBackend.java
@@ -2,6 +2,11 @@ package com.ducishere.hyperworldgen.world.noise;
 
 public class HybridBackend {
 
+    @param x Y world
+    @param y Y world
+    @param z Z world
+    @return
+
     public static double sampleAll(double x, double y, double z){
         double base = OpenSimplexBackend.sample(x,y,z);
         double mountain = RidgedBackend.sample(x,y,z);
@@ -10,7 +15,20 @@ public class HybridBackend {
         double terrace = TerraceBackend.sample(x,y,z);
         double river = RiverBackend.sample(x,y,z);
         double fast = FastNoiseBackend.sample(x,y,z);
+        double terra = TerraBlendBackend.sample(x,y,z);
         // Blend tất cả, weight tùy chỉnh
-        return base*0.3 + mountain*0.25 + cave*0.1 + warp*0.1 + terrace*0.1 + river*0.05 + fast*0.1;
+
+        double value = 0;
+        value += base*0.2;
+        value += mountain*0.25;
+        value += warp*0.1;
+        value += cave*0.1;
+        value += terrace*0.1;
+        value += river*0.05;
+        value += fast*0.1;
+        value += terra*0.1;
+
+        return value;
+        
     }
 }
