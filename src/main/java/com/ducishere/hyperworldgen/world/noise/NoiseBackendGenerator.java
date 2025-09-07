7c078565e2dbf450b1e509e31ab4b11d44cac440
DucIsHere <thelegendwolf07@gmail.com>
2025-09-07 12:40:27 +0700

    Update NoiseBackendGenerator.java

--- src/main/java/com/ducishere/hyperworldgen/world/noise/NoiseBackendGenerator.java
+++ src/main/java/com/ducishere/hyperworldgen/world/noise/NoiseBackendGenerator.java
@@ -1,18 +1,18 @@
-package com.ducishere.hyperworldgen.world.noise;
-
 public class NoiseBackendManager {
 
-    public static double sample(String noiseName, double x, double y, double z) {
-        switch(noiseName) {
-            case "opensimplex": return OpenSimplexBackend.sample(x, y, z);
-            case "ridged":     return RidgedBackend.sample(x, y, z);
-            case "cellular":   return CellularBackend.sample(x, y, z);
-            case "warp":       return DomainWarpBackend.sample(x, y, z);
-            case "terrace":    return HybridBackend.sampleTerrace(x, y, z);
-            case "river":      return HybridBackend.sampleRiver(x, y, z);
-            case "cave":       return HybridBackend.sampleCave(x, y, z);
-            case "ocean":      return TerraBlendBackend.sample(x, y, z);
-            default: return 0.0;
+    public static double sample(String name, double x, double y, double z) {
+        switch(name) {
+            case "opensimplex": return OpenSimplexBackend.sample(x,y,z);
+            case "ridged": return RidgedBackend.sample(x,y,z);
+            case "cellular": return CellularBackend.sample(x,y,z);
+            case "domainwarp": return DomainWarpBackend.sample(x,y,z);
+            case "terrace": return TerraceBackend.sample(x,y,z);
+            case "river": return RiverBackend.sample(x,y,z);
+            case "cave": return CaveBackend.sample(x,y,z);
+            case "fastnoise": return FastNoiseBackend.sample(x,y,z);
+            case "terrablend": return TerraBlendBackend.sample(x,y,z);
+            case "hybrid": return HybridBackend.sampleAll(x,y,z); // blend tất cả
+            default: return 0;
         }
     }
 }
