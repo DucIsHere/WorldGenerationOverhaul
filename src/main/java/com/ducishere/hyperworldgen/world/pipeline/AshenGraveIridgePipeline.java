6eb615ba794c37a2ab39ca461c9a1c097647203b
HoangNotRiel[流浪者] <dle145171@outlook.com>
2025-09-14 12:49:01 +0700

    Update and rename AshenGraveIridge.java to AshenGraveIridgePipeline.java
    Signed-off-by: HoangNotRiel[流浪者] <dle145171@outlook.com>

--- src/main/java/com/ducishere/hyperworldgen/world/pipeline/AshenGraveIridgePipeline.java
+++ src/main/java/com/ducishere/hyperworldgen/world/pipeline/AshenGraveIridgePipeline.java
@@ -2,7 +2,7 @@ package com.ducishere.hyperworldgen.world.pipeline;
 
 import com.ducishere.hyperworldgen.world.noise.base.*;
 
-public class AshenGraveIridge {
+public class AshenGraveIridgePipeline {
   public static double simple(double x, double y, double z, long seed) {
     double dune = new DuneBackend(seed, 0.03, 0.8).sample(x,y,z);
     double ridged = new RidgedBackend(seed, 0.05, 0.4).sample(x,y,z);
