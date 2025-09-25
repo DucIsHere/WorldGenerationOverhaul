eb0d0076abbfca3a1a229b8d42a384c40885bbd9
DucIsHere <thelegendwolf07@gmail.com>
2025-09-25 21:35:54 +0700

    Update HyperRegion.java
    Signed-off-by: DucIsHere <thelegendwolf07@gmail.com>

--- src/main/java/com/ducishere/hyperworldgen/world/region/HyperRegion.java
+++ src/main/java/com/ducishere/hyperworldgen/world/region/HyperRegion.java
@@ -1,9 +1,9 @@
-package com.ducishere.hyperworldgen.world.region;
+package com.hypergenworld.world.region;
 
+import com.hypergenworld.world.biome.ModBiomes;
+import com.hypergenworld.world.parameters.*;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.source.Climate;
 import terrablender.api.Region;
@@ -12,20 +12,16 @@ import java.util.function.Consumer;
 
 public class HyperRegion extends Region {
 
-    public HyperRegion(Identifier name, int weight) {
-        super(name, RegionType.OVERWORLD, weight);
+    public HyperRegion() {
+        super(new net.minecraft.util.Identifier("hypergenworld", "overworld"), RegionType.OVERWORLD, 5);
     }
 
     @Override
     public void addBiomes(RegistryKey<Biome> registry, Consumer<Pair<Climate.ParameterPoint, RegistryKey<Biome>>> mapper) {
-        Climate.ParameterPoint point = Climate.parameters(
-                Climate.Parameter.span(-1.0F, 1.0F),
-                Climate.Parameter.span(-1.0F, 1.0F),
-                Climate.Parameter.span(-1.0F, 1.0F),
-                Climate.Parameter.span(-1.0F, 1.0F),
-                Climate.Parameter.point(0.0F),
-                Climate.Parameter.span(-1.0F, 1.0F)
-        );
-        mapper.accept(Pair.of(point, registry));
+        // Gắn từng parameter với từng biome
+        mapper.accept(Pair.of(GlacialPinewoodParameters.PARAMETER, ModBiomes.GLACIAL_PINEWOOD));
+        mapper.accept(Pair.of(BlizzardHellParameters.PARAMETER, ModBiomes.BLIZZARD_HELL));
+        mapper.accept(Pair.of(PlainsBirchParameters.PARAMETER, ModBiomes.PLAINS_BIRCH));
+        // ... add thêm ở đây
     }
 }
